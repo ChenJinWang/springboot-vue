5646835f4391f66a069851be5f86812063666f29
ChenJinWang <2322237744@qq.com>
2022-07-03 23:23:59 +0800

    实现用户管理部分

--- src/main/java/com/example/config/WebAppConfigurer.java
+++ src/main/java/com/example/config/WebAppConfigurer.java
@@ -3,6 +3,7 @@ package com.example.config;
 import com.example.interceptor.SysInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,6 +13,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @Configuration
 public class WebAppConfigurer implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowCredentials(true)
+                .allowedMethods("GET", "HEAD", "POST", "PUT",
+                        "DELETE","OPTIONS")
+                .maxAge(3600);
+    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {    //addResourceHandlers资源处理
